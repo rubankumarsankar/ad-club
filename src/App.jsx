2c1f7f66f36a5b526fbc7c82bdcce71aaab400fc
rubankumarsankar <rubankumarsankar@users.noreply.github.com>
2025-05-21 18:22:42 +0530

    Update New 21/05/2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import Community from './components/com';
+
 import CommunityBanner from './components/CommunityBanner';
 import DiscoverMoments from './components/DiscoverMoments';
 import EventSection from './components/EventSection';
-import FooterBanner from './components/foot';
+
 import FooterSection from './components/FooterSection';
 import HeroSection from './components/HeroSection';
 import JoinClubSection from './components/JoinClubSection';
-import JoinUsBanner from './components/JoinUsBanner';
+
 import JoinUsSection from './components/JoinUsSection';
 import LatestNews from './components/LatestNews';
 import LegacySection from './components/LegacySection';
-import LegacyHeroSection from './components/new';
+
 import UpcomingEvents from './components/UpcomingEvents';
 import WhyJoinUsBanner from './components/WhyJoinUsBanner';
 import { ThemeProvider } from './context/ThemeContext';
